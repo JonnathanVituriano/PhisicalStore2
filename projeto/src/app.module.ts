ef2c104e4c935745bb4fb738148693734522cd14
Jonnathan Vituriano <viturianojonnathan@gmail.com>
2025-01-13 02:45:55 -0300

    resolvendo pastas duplicadas

--- projeto/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DatabaseModule } from './config/database';
-
-@Module({
-  imports: [DatabaseModule],
-  controllers: [],
-  providers: [],
-})
-export class AppModule {}
--- projeto/src/config/database.ts
+++
@@ -1,6 +0,0 @@
-import { MongooseModule } from '@nestjs/mongoose';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
-
-export const DatabaseModule = MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://localhost:27017/physical-store');
